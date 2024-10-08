6cc9e107df22fa92f127dbdbb7a209e98f8d7e7e
Luncy-May <cheng.li1@wustl.edu>
2024-10-07 23:15:36 -0500

    add state  pictures

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Report } from './pages'
-
+// npm i -S react-simple-maps
 function App() {
   return (
     <div className='bg-slate-600 min-h-screen min-w-screen text-white'>
